6636d2b47922a89d9c969e20d35b7bb5793cbbcf
Ty Thanh Doan <tdoan35@gmail.com>
2021-04-13 23:13:01 -0700

    worked on svgIcons

--- client/styles/Hero.js
+++ client/styles/Hero.js
@@ -28,9 +28,23 @@ export default makeStyles((theme) => ({
     fontWeight: 500,
     marginRight: 525
   },
-  cube: {
+  topCube: {
     position: 'absolute',
-    right: 0,
     backgroundColor: 'rgba(0, 0, 0, 0)',
-  }
+    top: 215,
+    left: '67.89%',
+    zIndex: 1,
+  },
+  rightCube: {
+    position: 'absolute',
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+    top: 400,
+    left: '67.89%',
+  },
+  leftCube: {
+    position: 'absolute',
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+    top: 400,
+    right: '32%',
+  },
 }));
